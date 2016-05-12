88e81ee065e8cca526dc8de5ab4bbf6f84f39ed4
Tiger Oakes <tigeroakes@gmail.com>
2016-05-12 13:09:54 -0700

    Added jsconfigs and typings data

---
+++ packages/api/backend/typings/browser.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="browser/ambient/knex/index.d.ts" />
+/// <reference path="browser/ambient/koa/index.d.ts" />
+/// <reference path="browser/ambient/node/index.d.ts" />
---
+++ packages/api/backend/typings/main.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="main/ambient/knex/index.d.ts" />
+/// <reference path="main/ambient/koa/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
