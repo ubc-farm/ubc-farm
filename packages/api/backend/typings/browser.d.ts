d1eb3c8761a43478fbffca8d1e0c8e9a3c8edd4e
Tiger Oakes <tigeroakes@gmail.com>
2016-05-13 11:27:09 -0700

    Added bluebird typings

--- packages/api/backend/typings/browser.d.ts
+++ packages/api/backend/typings/browser.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="browser/ambient/bluebird/index.d.ts" />
 /// <reference path="browser/ambient/knex/index.d.ts" />
 /// <reference path="browser/ambient/koa/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
--- packages/api/backend/typings/main.d.ts
+++ packages/api/backend/typings/main.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="main/ambient/bluebird/index.d.ts" />
 /// <reference path="main/ambient/knex/index.d.ts" />
 /// <reference path="main/ambient/koa/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
