cf2f9eedaf8a29ef828944c8eb88e89cdd44bb97
Tiger Oakes <tigeroakes@gmail.com>
2016-05-17 16:04:06 -0700

    Updated with animation for icon-label elements

--- packages/api/views/_taglib/icon-label/renderer.js
+++ packages/api/views/_taglib/icon-label/renderer.js
@@ -4,13 +4,15 @@ const iconHelper = require('../../_helpers/icon.js')
 const template = require('./template.marko');
 
 exports.renderer = (input, out) => {
-	let {text, icon, href} = input;
+	let {text, icon, href, tab, active} = input;
 	
 	href = path.join('/', href);
 	
 	template.render({
 		text: text,
 		icon: iconHelper.format(icon),
-		href: href
+		href: href,
+		tabindex: (tab? 0 : false),
+		active: active
 	}, out);
 }
