6afaa1cb7779e443bf9830f629894301d27a0f50
Tiger Oakes <tigeroakes@gmail.com>
2016-05-19 10:01:17 -0700

    Set up code to restrict client javascript to a namespace to avoid pollution globals

--- packages/api/frontend/utils.js
+++ packages/api/frontend/utils.js
@@ -1,11 +1,29 @@
 /**
- * Resolves when the DOM is ready to interact with
- * @type {Promise<>}
+ * Namespace for UBC Farm scripts
+ * @namespace
  */
-var domReady = new Promise(function(resolve) {
-	function checkState() {
-		if (document.readyState != 'loading') resolve();
+var farm = {
+	/**
+	 * Namespace for functions that create elements
+	 * @namespace
+	 */
+	element: {},
+	
+	/**
+	 * Namespace for utlity functions
+	 * @namespace
+	 */
+	utils: {
+		/**
+		 * Resolves when the DOM is ready to interact with
+		 * @type {Promise<void>}
+		 */
+		domReady: new Promise(function(resolve) {
+			function checkState() {
+				if (document.readyState != 'loading') resolve();
+			}
+			document.addEventListener('readystatechange', checkState);
+			checkState();
+		})
 	}
-	document.addEventListener('readystatechange', checkState);
-	checkState();
-})
+}
