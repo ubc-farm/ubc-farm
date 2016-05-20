aac462a2d2a50fa3f34a4720abf764c7f457e3ed
Tiger Oakes <tigeroakes@gmail.com>
2016-05-20 09:49:32 -0700

    Added JSDocs for the icon- components

--- packages/api/views/_taglib/icon-button/renderer.js
+++ packages/api/views/_taglib/icon-button/renderer.js
@@ -2,6 +2,14 @@ const iconHelper = require('../../_helpers/icon.js')
 
 const template = require('./template.marko');
 
+/**
+ * @param {Object} input
+ * @param {string} [input.icon] - icon shown to the left of the text
+ * @param {string} [input.label] - text shown on the button
+ * @param {number} [input.size=24] - width and height for the icon
+ * @param {string} [input.id] - id for the button
+ * @param {string} [input.class] - addtional classes for the button
+ */
 exports.renderer = (input, out) => {
 	let className = input.class ? ' ' + input.class : '';
 	let {icon, label, size, id} = input;
--- packages/api/views/_taglib/icon-tab/renderer.js
+++ packages/api/views/_taglib/icon-tab/renderer.js
@@ -2,6 +2,13 @@ const iconHelper = require('../../_helpers/icon.js');
 
 const template = require('./template.marko');
 
+/**
+ * @param {Object} input
+ * @param {string} input.text - text shown on the tab
+ * @param {string} input.icon - icon name, passed to iconHelper
+ * @param {string} input.href - url for the tab's link
+ * @param {boolean} input.active - does this tab represent the current page?
+ */
 exports.renderer = (input, out) => {
 	let {text, icon, href, active} = input;
 	let thisC = active ? "this" : "";
