017bd94b7a3a143296618fe393733cfe63f28f0e
Tiger Oakes <tigeroakes@gmail.com>
2016-05-20 09:50:09 -0700

    Completed initial work on bar-chart component

--- packages/api/views/_taglib/bar-chart/renderer.js
+++ packages/api/views/_taglib/bar-chart/renderer.js
@@ -1,16 +1,33 @@
-const iconHelper = require('../../_helpers/icon.js');
-
 const template = require('./template.marko');
+const money = require('../../_helpers/money.js')
 
+/**
+ * @typedef {Object} ChartRow
+ * @property {string} name - name of the object that this bar belongs to
+ * @property {string} [url] - url for more info about the object
+ * @property {number} revenue in cents
+ * @property {integer} expenses in cents
+ */
+
+/**
+ * @param {Object} input
+ * @param {ChartRow[]} input.data - data for chart
+ * @param {string} input.color - class that indicates color for chart
+ */
 exports.renderer = (input, out) => {
-	let {text, icon, href, active} = input;
-	let thisC = active ? "this" : "";
-	href = href? href : '/' + text.toLowerCase();
+	let {data, color} = input;
+	
+	for (row in data) {
+		let {revenue, expenses} = row;
+		let profit = revenue - expenses;
+		
+		row.revenue = money.toMoney(revenue);
+		row.expenses = money.toMoney(expenses);
+		row.profit = money.toMoney(profit);
+	}
 	
 	template.render({
-		text: text,
-		icon: iconHelper.format(icon),
-		href: href,
-		className: "inline icon-text i-tab" + thisC;
+		data: data,
+		color: color
 	}, out);
 }
