fe49d0817dc3c9fb493f3fd066d31b4e4f749b71
Tiger Oakes <tigeroakes@gmail.com>
2016-08-15 16:18:42 -0700

    Added redux template

---
+++ packages/page-directory/src/redux/reducer-simple.js
@@ -0,0 +1,8 @@
+import {} from './actions.js'
+
+export default function loading(state = {}, action) {
+	const setState = newState => Object.assign({}, state, newState);
+	switch (action.type) {
+		default: return state;
+	}
+}
