4a5e343b86927e1ae29343c733e8c57b498b8b95
Tiger Oakes <tigeroakes@gmail.com>
2016-08-15 14:19:45 -0700

    Removed more old files

--- packages/views/server/database-routes/transformer-validation.js
+++
@@ -1,7 +0,0 @@
-const Joi = require('joi') //eslint-disable-line import/no-commonjs
-
-export const print = Joi.string().valid('silent', 'pretty');
-
-export const shallow = Joi.boolean();
-
-export const validate = {query: Joi.object({print, shallow})};
