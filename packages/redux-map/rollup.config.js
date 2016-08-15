6bd37a626a9ef66abdabdc64db07afdd9a18d4fc
Tiger Oakes <tigeroakes@gmail.com>
2016-08-15 16:01:19 -0700

    General project structure for page modules

---
+++ packages/redux-map/rollup.config.js
@@ -0,0 +1,28 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+
+export default {
+	entry: 'src/index.js',
+	sourceMap: true,
+	targets: [
+		{ dest: 'index.iife.js', format: 'iife' }
+	],
+	plugins: [
+		babel({
+			plugins: ['transform-react-jsx', 'external-helpers-2']
+		}),
+		nodeResolve({jsnext: true}),
+		replace({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
+		commonjs()
+	],
+	external: ['react', 'react-dom', 'tape'],
+	globals: {
+		'react': 'React',
+		'react-dom': 'ReactDOM',
+		'tape': 'test'
+	}
+}
