d790c40ec7dc9da901ef83620612d1ff8fccfa4b
Tiger Oakes <tigeroakes@gmail.com>
2016-08-15 15:11:05 -0700

    Updated with css-partials route

--- packages/views/server/index.js
+++ packages/views/server/index.js
@@ -4,6 +4,7 @@ import {resolve} from 'path';
 
 import analytics from './routes/analytics.js';
 import coreCss from './routes/css-core.js';
+import partialCss from './routes/css-core.js';
 
 const connection = {
 	port: process.env.npm_package_config_port
@@ -15,7 +16,8 @@ server.connection(connection);
 server.path(resolve(__dirname, '../'));
 server.register(Inert, err => {if (err) throw err});
 
-server.route(coreCss);
 server.route(analytics);
+server.route(coreCss);
+server.route(partialCss);
 
 export default server;
---
+++ packages/views/server/routes/css-partials.js
@@ -0,0 +1,12 @@
+export default {
+	method: 'GET',
+	path: '/css/partials/{param}',
+	handler: {
+		directory: {
+			path: 'node_modules/ubc-farm-views-utils/partials/css',
+			listing: true,
+			defaultExtension: 'css',
+			index: false
+		}
+	}
+}
