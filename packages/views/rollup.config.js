34536c90ccbdbca63d83b103fa66ac4d6efe1ccd
Tiger Oakes <tigeroakes@gmail.com>
2016-08-15 14:23:08 -0700

    Updated package.json

--- packages/views/rollup.config.js
+++ packages/views/rollup.config.js
@@ -2,8 +2,8 @@ export default {
 	entry: 'index.js',
 	sourceMap: true,
 	targets: [
-		{ dest: 'dist/index.node.js', format: 'cjs' },
-		{ dest: 'dist/index.es.js', format: 'es' }
+		{ dest: 'index.node.js', format: 'cjs' },
+		{ dest: 'index.es.js', format: 'es' }
 	],
 	external: [
 		'hapi', 'inert',
