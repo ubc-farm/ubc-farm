cdf4827aaa1a5862955b4d4a29d24079aa5cfbeb
Tiger Oakes <tigeroakes@gmail.com>
2016-08-15 14:21:06 -0700

    Add css route and server

---
+++ packages/static/server/routes/css-core.js
@@ -0,0 +1,26 @@
+export const core = {
+	method: 'GET',
+	path: '/css/core/{param*}',
+	handler: {
+		directory: {
+			path: 'node_modules/ubc-farm-css/src',
+			listing: true,
+			defaultExtension: 'css',
+			index: false
+		}
+	}
+}
+
+export const coreMin = {
+	method: 'GET',
+	path: '/css/core/min.css',
+	handler: {
+		file: 'node_modules/ubc-farm-css/min.css'
+	}
+}
+
+export const coreIndex = Object.assign({}, coreMin, {
+	path: '/css/core/index.css'
+});
+
+export default [core, coreMin, coreIndex];
