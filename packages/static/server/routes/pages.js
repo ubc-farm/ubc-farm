8e142a13e4322bc930138ce15e8892f35b9cd16b
Tiger Oakes <tigeroakes@gmail.com>
2016-08-16 14:49:38 -0700

    Added custom index files

--- packages/static/server/routes/pages.js
+++ packages/static/server/routes/pages.js
@@ -13,19 +13,34 @@ function cssRoute(pagename) {
 	}
 }
 
+/**
+ * @todo better resolution algorithm
+ */
 function javascriptRoute(pagename) {
-	return {
-		method: 'GET',
-		path: `/js/page/${pagename}/{param*}`,
-		handler: {
-			directory: {
-				path: `node_modules/ubc-farm-page-${pagename}/dist`,
-				listing: true,
-				defaultExtension: 'js',
-				index: false
+	const pkg = require(`node_modules/ubc-farm-page-${pagename}/package.json`);
+	const {browser: main = 'dist/index.iife.js'} = pkg;
+
+	return [
+		{
+			method: 'GET',
+			path: `/js/page/${pagename}/{param*}`,
+			handler: {
+				directory: {
+					path: `node_modules/ubc-farm-page-${pagename}/dist`,
+					listing: true,
+					defaultExtension: 'js',
+					index: false
+				}
+			}
+		},
+		{
+			method: 'GET',
+			path: `/js/page/${pagename}/index.js`,
+			handler: {
+				file: `node_modules/ubc-farm-page-${pagename}/${main}`
 			}
 		}
-	}
+	]
 }
 
 const pageList = [
