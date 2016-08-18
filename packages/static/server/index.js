f8f3457444164d438ddee0a6ac7cd2c358efe223
Tiger Oakes <tigeroakes@gmail.com>
2016-08-18 10:26:14 -0700

    Added and enabled module routes for the couple packages that have CSS that needs to be served

--- packages/static/server/index.js
+++ packages/static/server/index.js
@@ -10,6 +10,7 @@ import coreCss from './routes/css-core.js';
 import partialCss from './routes/css-partials.js';
 
 import pageRoutes from './routes/pages.js';
+import moduleRoutes from './routes/modules.js';
 
 const server = new Server();
 server.connection(connection);
@@ -22,5 +23,6 @@ server.route(coreCss);
 server.route(partialCss);
 
 pageRoutes.then(pages => server.route(pages));
+moduleRoutes.then(mods => server.route(mods));
 
 export default server;
--- packages/static/server/routes/modules.js
+++ packages/static/server/routes/modules.js
@@ -1,12 +1,69 @@
-export const tableBase = {
-	method: 'GET',
-	path: '/css/module/table-base/{param*}',
+import {join} from 'path';
+import __dirname from '../../dirname.js'
+import search from '../find-folder.js';
+
+const folder = join(__dirname, 'node_modules');
+function searchModule(name) {
+	return search(folder, `ubc-farm-${name}`, name);
+}
+
+const method = 'GET', listing = true, index = false;
+
+const tableBase = searchModule('table-base').then(folder => ({
+	method, path: '/css/module/table-base/{param*}',
 	handler: {
 		directory: {
-			path: 'node_modules/ubc-farm-table-base/styles',
-			listing: true,
-			defaultExtension: 'css',
-			index: false
+			path: join(folder, 'styles'),
+			listing, index,
+			defaultExtension: 'css'
 		}
 	}
-}
+}))
+
+const datetime = searchModule('datetime-picker').then(folder => ({
+	method, path: '/css/module/datetime-picker/{param*}',
+	handler: {
+		directory: {
+			path: join(folder, 'styles'),
+			listing, index,
+			defaultExtension: 'css'
+		}
+	}
+}))
+
+const ports = searchModule('ports').then(folder => {
+	const tape = {
+		method, path: '/js/module/tape/{file?}',
+		handler: {
+			file: join(folder, 'tape/index.js')
+		}
+	};
+
+	const visTimelineCss = {
+		method, path: '/css/module/vis-timeline/{param*}',
+		handler: {
+			directory: {
+				path: join(folder, 'vis-timeline/dist'),
+				index, listing: false,
+				defaultExtension: 'css'
+			}
+		}
+	}
+
+	const visTimelineJs = {
+		method,
+		path: '/js' + visTimelineCss.path.substr('/css'.length),
+		handler: {
+			directory: Object.assign({}, visTimelineCss.handler.directory, {
+				defaultExtension: 'js'
+			})
+		}
+	}
+
+	return [tape, visTimelineCss, visTimelineJs];
+})
+
+const all = Promise.all([tableBase, datetime, ports])
+	.then( routes => routes.reduce((a = [], b) => [...a, ...b]) );
+
+export default all;
