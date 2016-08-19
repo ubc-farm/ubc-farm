f6d23a9c526be81a5a7c85c02b2ce58884d12a18
Tiger Oakes <tigeroakes@gmail.com>
2016-08-19 13:24:23 -0700

    Added money class code

--- packages/page-add-items/src/submit-item.js
+++ packages/page-add-items/src/submit-item.js
@@ -1,6 +1,14 @@
 import {SubmissionError} from 'redux-form';
+import Money from 'ubc-farm-utils/class/money/improved.js';
 
 export default function submit(data) {
+	if (data.value !== undefined) 
+		data.value = new Money(data.value);
+	if (data.salvageValue !== undefined) 
+		data.salvageValue = new Money(data.salvageValue);
+	
+	console.log(data);
+
 	return fetch('/api/items', {
 		method: 'POST',
 		body: JSON.stringify(data)
