c5c0e63f38c4ed3de34293cba5b7e503c7c2b764
Tiger Oakes <tigeroakes@gmail.com>
2016-08-19 12:24:09 -0700

    startServer now returns Promise

--- packages/money/function/start-server.js
+++ packages/money/function/start-server.js
@@ -3,7 +3,7 @@
 export default function startServer(server, name) {
 	process.title = name;
 
-	server.start().then(() => {
+	return server.start().then(() => {
 		console.log(`[+] ${name} server running at: ${server.info.uri}`);
 	}).catch(err => {
 		console.error(`[X] ${name} server issue: ${err}`)
