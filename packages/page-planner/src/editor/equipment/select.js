d460a7e21071df4eb39f8bc4d73b067f70858903
Tiger Oakes <tigeroakes@gmail.com>
2016-08-25 11:53:35 -0700

    Adding eqiuipment selector

---
+++ packages/page-planner/src/editor/equipment/select.js
@@ -0,0 +1,18 @@
+import { createElement as h, PropTypes } from 'react'; /** @jsx h */
+
+const EquipmentSelector = ({ value, onChange, options }) => (
+	<select value={value} onChange={onChange}>
+		<option value="" disabled />
+		{Array.from(options, ([id, { name }]) => (
+			<option value={id} key={id}>{name}</option>
+		))}
+	</select>
+);
+
+EquipmentSelector.propTypes = {
+	options: PropTypes.instanceOf(Map),
+	value: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
+};
+
+export default EquipmentSelector;
--- packages/page-planner/src/editor/time/connected.js
+++ packages/page-planner/src/editor/time/connected.js
@@ -6,7 +6,7 @@ export default connect(
 	state => {
 		/* eslint-disable camelcase */
 		const { start_time, end_time } = selectedTaskObject(state) || {};
-		console.log(start_time);
+
 		return {
 			start: start_time != null ? new Date(start_time) : undefined,
 			end: end_time != null ? new Date(end_time) : undefined,
