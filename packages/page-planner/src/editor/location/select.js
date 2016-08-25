730c4991d7d06684a2e8a6c39229b58a2702a95d
Tiger Oakes <tigeroakes@gmail.com>
2016-08-25 10:35:37 -0700

    Working on location selector

--- packages/page-planner/src/editor/location/select.js
+++ packages/page-planner/src/editor/location/select.js
@@ -1,24 +1,51 @@
-import { createElement as h } from 'react'; /** @jsx h */
+import { createElement as h, PropTypes } from 'react'; /** @jsx h */
 import { connect } from 'react-redux';
-import RelationSelect from 'ubc-farm-inputs/relation.js';
-import { locationsList } from '../../redux/selectors.js';
+import { createSelector } from 'reselect';
+
+import {
+	locationsList,
+	selectedTask,
+	taskListSelector,
+} from '../../redux/selectors.js';
 import { setSelectedLocation } from '../../redux/actions/index.js';
 
-const LocationSelect = props => (
-	<RelationSelect url="/api/locations" input={props} />
+const LocationSelect = ({ value, disabled, onChange, options }) => (
+	<select {...{ value, disabled, onChange }}>
+		<option value="" disabled />
+		{Array.from(options, ([id, { name }]) => (
+			<option value={id} key={id}>{name}</option>
+		))}
+	</select>
+);
+
+LocationSelect.propTypes = {
+	options: PropTypes.instanceOf(Map),
+	value: PropTypes.string.isRequired,
+	disabled: PropTypes.bool,
+	onChange: PropTypes.func.isRequired,
+};
+
+
+const selectedTaskObject = createSelector(
+	selectedTask,
+	taskListSelector,
+	(selectedId, list) => list.get(selectedId)
 );
 
 export default connect(
-	state => ({
-		value: locationsList(state),
-	}),
-	dispatch => ({
-		onChange(e) {
-			let value;
-			if (typeof e !== 'object') value = e;
-			else value = e.target.value;
+	state => {
+		const selected = selectedTaskObject(state);
+		let value = '';
+		let disabled = false;
 
-			dispatch(setSelectedLocation(value));
+		if (selected != null) value = selected.locationId;
+		else disabled = true;
+
+		return { value, disabled, options: locationsList(state) };
+	},
+	dispatch => ({
+		onChange({ target }) {
+			dispatch(setSelectedLocation(target.value));
 		},
 	})
 )(LocationSelect);
