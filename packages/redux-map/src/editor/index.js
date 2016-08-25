22e7c47e50d37a4de42e4649903b92ff6f49729a
Tiger Oakes <tigeroakes@gmail.com>
2016-08-25 10:35:37 -0700

    Working on location selector

--- packages/redux-map/src/editor/index.js
+++ packages/redux-map/src/editor/index.js
@@ -1,1 +1,12 @@
-export default () => null; // TODO
+import { createElement } from 'react'; /** @jsx createElement */
+
+import LocationSelect from './location/select.js';
+
+const EditorPanel = () => (
+	<form>
+		<h3>Location</h3>
+		<LocationSelect />
+	</form>
+);
+
+export default EditorPanel;
