8fdfd6de5f928ce984fc4ed4fa1c7a6c14a97041
Tiger Oakes <tigeroakes@gmail.com>
2016-08-26 13:14:49 -0700

    Bugfixing + moved EquipmentCount valiadation to input.max attribute rather than checking the count manually

--- packages/page-planner/src/editor/equipment/count.js
+++ packages/page-planner/src/editor/equipment/count.js
@@ -1,21 +1,20 @@
 import { createElement as h, PropTypes } from 'react'; /** @jsx h */
-import { classlist as cx } from 'ubc-farm-utils';
 import { connect } from 'react-redux';
 import { setSelectedEquipment } from '../../redux/actions/index.js';
 import { selectedEquipment, equipmentList } from '../../redux/selectors.js';
 
-const EquipmentCount = ({ value = '', onChange, invalid }) => (
+const EquipmentCount = ({ value = '', onChange, max }) => (
 	<input
 		type="number"
-		className={cx('equip-count', { invalid })}
+		className="equip-count"
 		value={value} onChange={onChange}
-		data-error={invalid}
+		max={max}
 		placeholder="0"
 	/>
 );
 
 EquipmentCount.propTypes = {
-	invalid: PropTypes.string,
+	max: PropTypes.number,
 	value: PropTypes.string,
 	onChange: PropTypes.func.isRequired,
 };
@@ -27,13 +26,10 @@ export default connect(
 
 		if (!equipment.has(id)) return { value: count };
 
-		const { quantity } = equipment.get(id);
-		let invalid;
-		if (quantity != null && quantity < count) {
-			invalid = `There are only ${quantity} of this item`;
-		}
-
-		return { value: count, invalid };
+		return {
+			value: count,
+			max: equipment.get(id).quantity,
+		};
 	},
 	(dispatch, { position }) => ({
 		onChange({ target }) {
