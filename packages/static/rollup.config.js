bc51f159f1c9440a8082f53763d4d80312479251
Tiger Oakes <tigeroakes@gmail.com>
2016-08-29 10:18:45 -0700

    Added node-resolve plugin

--- packages/static/rollup.config.js
+++ packages/static/rollup.config.js
@@ -1,14 +1,18 @@
 import json from 'rollup-plugin-json';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
 	entry: 'index.js',
 	sourceMap: true,
 	targets: [
-		{ dest: 'index.node.js', format: 'cjs' }
+		{ dest: 'index.node.js', format: 'cjs' },
 	],
 	external: [
 		'hapi', 'inert',
-		'path', 'tape'
+		'path', 'tape', 'fs',
 	],
-	plugins: [json()]
+	plugins: [
+		json(),
+		nodeResolve({ jsnext: true }),
+	],
 };
