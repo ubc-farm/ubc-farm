f27e43620e6942c3cc4238aae0a0dea9b4474d0d
Tiger Oakes <tigeroakes@gmail.com>
2016-08-29 10:59:51 -0700

    Fixed paths

--- packages/static/server/routes/index.js
+++ packages/static/server/routes/index.js
@@ -1,8 +1,8 @@
-import analytics from './routes/analytics.js';
-import partialCss from './routes/css-partials.js';
-import coreCss from './routes/css-core.js';
-import pageRoutes from './routes/pages.js';
-import moduleRoutes from './routes/modules.js';
+import analytics from './analytics.js';
+import partialCss from './css-partials.js';
+import coreCss from './css-core.js';
+import pageRoutes from './pages.js';
+import moduleRoutes from './modules.js';
 
 export default Promise.all([
 	analytics,
