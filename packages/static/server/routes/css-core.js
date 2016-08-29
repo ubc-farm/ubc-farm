28f5f14bb7d19ade6c45707a2ad27dd1da9cad14
Tiger Oakes <tigeroakes@gmail.com>
2016-08-29 10:58:39 -0700

    Changed css-core to search for its path

--- packages/static/server/routes/css-core.js
+++ packages/static/server/routes/css-core.js
@@ -1,26 +1,37 @@
-export const core = {
-	method: 'GET',
-	path: '/css/core/{param*}',
-	handler: {
-		directory: {
-			path: 'node_modules/ubc-farm-css/src',
-			listing: true,
-			defaultExtension: 'css',
-			index: false,
-		},
-	},
-};
+import { resolve } from 'path';
+import __dirname from '../../dirname.js';
+import search from '../find-folder.js';
 
-export const coreMin = {
-	method: 'GET',
-	path: '/css/core/min.css',
-	handler: {
-		file: 'node_modules/ubc-farm-css/min.css',
-	},
-};
-
-export const coreIndex = Object.assign({}, coreMin, {
-	path: '/css/core/index.css',
+const cssCoreSearch = search(__dirname, 'node_modules/ubc-farm-css').then(path => {
+	if (path === undefined) throw new Error('Could not find css-core package');
+	return path;
 });
 
-export default [core, coreMin, coreIndex];
+export default cssCoreSearch.then(cssCore => {
+	const core = {
+		method: 'GET',
+		path: '/css/core/{param*}',
+		handler: {
+			directory: {
+				path: resolve(cssCore, '../src'),
+				listing: true,
+				defaultExtension: 'css',
+				index: false,
+			},
+		},
+	};
+
+	const coreMin = {
+		method: 'GET',
+		path: '/css/core/min.css',
+		handler: {
+			file: resolve(cssCore, '../min.css'),
+		},
+	};
+
+	const coreIndex = Object.assign({}, coreMin, {
+		path: '/css/core/index.css',
+	});
+
+	return [core, coreMin, coreIndex];
+});
