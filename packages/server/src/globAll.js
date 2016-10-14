f43f98a91c72ccf21d3068700b5b10e736c7d2f6
Tiger Oakes <tigeroakes@gmail.com>
2016-10-14 13:42:56 -0700

    Added globAll script

---
+++ packages/server/src/globAll.js
@@ -0,0 +1,48 @@
+import globCb from 'glob';
+
+/** Promisified glob function */
+function glob(pattern, options) {
+	return new Promise((resolve, reject) => {
+		globCb(pattern, options, (err, result) => {
+			if (err) return reject(err);
+			return resolve(result);
+		});
+	});
+}
+
+/**
+ * @param {Array<V>} array
+ * @param {V} removedItem
+ * @returns {Array<V>} a copy of the array with the given item removed.
+ */
+function restOfArray(array, removedItem) {
+	return array.filter(v => v !== removedItem);
+}
+
+/**
+ * Same effects as glob, but takes an array of patterns
+ * rather than a single pattern.
+ * @param {string[]} patterns
+ * @param {Object} [options] passed to glob
+ * @returns {Promise<string[]>} array of file paths found by glob
+ */
+export default function globAll(patterns, options = {}) {
+	let symlinks;
+	let statCache;
+	let realpathCache;
+	let cache;
+
+	function addToIgnore(items) {
+		return Object.assign({}, options, {
+			ignore: options.ignore ? items.conat(options.ignore) : items,
+			symlinks,
+			statCache,
+			realpathCache,
+			cache,
+		});
+	}
+
+	return Promise.all(patterns.map((pattern, i, array) =>
+		glob(pattern, addToIgnore(restOfArray(array, pattern)))
+	)).then(results => [].concat(...results));
+}
