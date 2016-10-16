04c1b5f3460437af3e08290c02254f412fdf6787
Tiger Oakes <tigeroakes@gmail.com>
2016-10-15 20:03:00 -0700

    Updated test command

--- packages/server/rollup.config.test.js
+++ packages/server/rollup.config.test.js
@@ -1,7 +1,6 @@
 import config from './rollup.config.js';
 
 export default {
-	entry: 'test/importPlugin.spec.js',
 	plugins: [...config.plugins],
 	format: 'cjs',
 	external: [...config.external, 'blue-tape'],
