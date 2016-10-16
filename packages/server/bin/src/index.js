8a790c56a48cd3facd542f2e7c738d0c2f09086c
Tiger Oakes <tigeroakes@gmail.com>
2016-10-15 19:56:18 -0700

    Updated CLI code with status messages

--- packages/server/bin/src/index.js
+++ packages/server/bin/src/index.js
@@ -1,7 +1,31 @@
 import minimist from 'minimist';
 // eslint-disable-next-line import/no-unresolved,import/no-extraneous-dependencies
-import { serverReady, searchPlugins } from 'ubc-farm-server';
+import server, { serverReady, searchPlugins } from 'ubc-farm-server';
+
+/* eslint-disable no-console */
 
 const { _: patterns } = minimist(process.argv.slice(2));
+searchPlugins(serverReady, patterns).then(failures =>
+	server.start().then(() => {
+		const { connections } = server;
+		if (connections.length === 1) {
+			console.log(`Server started at ${server.info.uri}`);
+		} else {
+			console.log('Server started with connections: ');
+			console.log(connections.map(c => c.info));
+		}
 
-searchPlugins(serverReady, patterns).then(server => server.start());
+		console.log('\nPlugins loaded:');
+		connections.forEach(({ plugins }) => console.log(plugins));
+
+		if (failures.size > 0) {
+			console.warn('\nCould not load plugins:');
+			for (const [path, err] of failures) {
+				console.warn(`${path} ${err.name}: ${err.message}`);
+			}
+		}
+	}).catch(err => {
+		console.error('Could not start server.');
+		console.error(err);
+	})
+);
