b17e32d64dd3340ad3ed94b3890f182fa5cc2900
Tiger Oakes <tigeroakes@gmail.com>
2016-10-29 12:37:08 -0700

    Updated package.json with scope address

--- packages/money/rollup.config.js
+++ packages/money/rollup.config.js
@@ -2,7 +2,7 @@ export default {
 	entry: 'src/index.js',
 	sourceMap: true,
 	targets: [
-		{ dest: 'dist/index.js', format: 'cjs' },
+		{ dest: 'dist/index.cjs.js', format: 'cjs' },
 		{ dest: 'dist/index.es.js', format: 'es' },
 	],
 };
