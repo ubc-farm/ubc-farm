3c6f65b1def9a83d11b45b6af575377cd19d22da
Tiger Oakes <tigeroakes@gmail.com>
2016-11-02 20:55:28 -0700

    Added CSV export code

--- packages/table-base/README.md
+++ packages/table-base/README.md
@@ -1,1 +1,5 @@
-# React Table
+# @ubc-farm/table-base
+
+Table component for React, with selection and sorting.
+
+TODO: filters, search, pagination, editing, insert, delete, export
---
+++ packages/table-base/src/manipulate/export.js
@@ -0,0 +1,46 @@
+/**
+ * Creates a CSV string from the given tableData and column elements
+ * @param {Object[]} tableData
+ * @param {ReactElement[]} columns
+ * @returns {string} csv formatted text
+ */
+export function toCSV(tableData, columns) {
+	const csvHead = columns.reduce((text, props) => {
+		if (props.hidden && props.export !== true) return text;
+		else if (props.export === false) return text;
+
+		let header = props.csvHeader || String(props.children) || '';
+		if (header.includes(',')) header = `"${header}"`;
+		header += ',';
+
+		return text + header;
+	}, '');
+
+	const rows = tableData.map(row => columns.reduce((text, props) => {
+		if (props.hidden && props.export !== true) return text;
+		else if (props.export === false) return text;
+
+		const cell = row[props.dataField];
+
+		let cellText = props.csvFormat ? props.csvFormat(cell, row) : String(cell);
+		if (cellText.includes(',')) cellText = `"${cellText}"`;
+		cellText += ',';
+		return text + cellText;
+	}));
+
+	return [csvHead, ...rows].reduce(
+		(file, line) => `${file}${line.slice(0, -1)}\n`
+	);
+}
+
+export function download(filename, text) {
+	const element = document.createElement('a');
+	element.setAttribute('href',
+		`data:text/plain;charset=utf-8,${encodeURIComponent(text)}`);
+	element.setAttribute('download', filename);
+	element.setAttribute('hidden', true);
+
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
+}
