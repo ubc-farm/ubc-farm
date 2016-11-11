9db5a78f3a84adf10eb587cc5c18d3ac4f2fde8b
Tiger Oakes <tigeroakes@gmail.com>
2016-11-11 00:45:19 -0800

    Added redux reducer to store the table data

---
+++ packages/page-add-items/src/redux/table.js
@@ -0,0 +1,45 @@
+const INIT = 'inventory/table/INIT';
+const ADD_ROW = 'inventory/table/ADD_ROW';
+const DELETE_ROW = 'inventory/table/DELETE_ROW';
+const EDIT_ROW = 'inventory/table/EDIT_ROW';
+
+export default function tableReducer(state = [], action = {}) {
+	switch (action.type) {
+		case INIT:
+			return action.payload;
+
+		case ADD_ROW:
+			return [...state, action.payload];
+
+		case DELETE_ROW: {
+			const copy = state.slice(0);
+			copy.splice(action.index, 1);
+			return copy;
+		}
+
+		case EDIT_ROW: {
+			const copy = state.slice(0);
+			copy[action.index] = action.payload;
+			return copy;
+		}
+
+		default:
+			return state;
+	}
+}
+
+
+/** @returns {Array<Array>} table data */
+export const getTable = store => store.table;
+/** @returns {Array} one column from the table */
+export const getColumn = (store, field) => store.table.map(row => row[field]);
+
+
+/** Sets the table to a blank slate with the given data */
+export const setTable = data => ({ type: INIT, payload: data });
+/** Adds a new row to the table */
+export const addRow = newRow => ({ type: ADD_ROW, payload: newRow });
+/** Deletes a row from the table */
+export const deleteRow = index => ({ type: DELETE_ROW, index });
+/** Replace a row in the table */
+export const editRow = (index, row) => ({ type: EDIT_ROW, index, payload: row });
