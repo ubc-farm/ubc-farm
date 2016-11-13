ffdfcd37521b1968722eb36fe74cdaf052a5c268
Tiger Oakes <tigeroakes@gmail.com>
2016-11-12 21:16:22 -0800

    Updated ribbon

--- packages/page-fields/src/toolbar/main.js
+++ packages/page-fields/src/toolbar/main.js
@@ -1,23 +1,22 @@
 import { createElement } from 'react'; /** @jsx createElement */
-import Ribbon, { TabList, Tab, ContextualTabs, Panel } from 'ribbon-toolbar';
+import Ribbon, { TabList, Tab, ContextualTabs, Panel, Barrier } from 'ribbon-toolbar';
 
 const Toolbar = () => (
 	<Ribbon>
-		<header className="ribbon-header">
+		<Barrier><header className="ribbon-header">
 			<h1 className="ribbon-title">Fields</h1>
-		</header>
-
+		</header></Barrier>
 		<TabList>
 			<Tab id="m">Map</Tab>
 			<ContextualTabs title="Field Options" hidden={false}>
 				<Tab id="g">Grid</Tab>
 			</ContextualTabs>
 		</TabList>
-
 		<Panel id="m"></Panel>
 		<Panel id="e"></Panel>
 		<Panel id="g"></Panel>
 	</Ribbon>
+
 );
 
 export default Toolbar;
