0ba5452086d3490418756e6e03588f1cc21e8be5
Tiger Oakes <tigeroakes@gmail.com>
2016-12-20 00:15:51 -0800

    Added prefix parameter to id()

--- packages/utils/lib/id.js
+++ packages/utils/lib/id.js
@@ -7,8 +7,11 @@
  * Convert it to base 36 (numbers + letters), and grab the first 9 characters
  * after the decimal.
  *
+ * @param {string} [prefix='_'] used to change the ID prefix. Use an empty string
+ * for no prefix.
  * @returns {string}
  * @see https://gist.github.com/gordonbrander/2230317
  */
-const id = () => `_${Math.random().toString(36).substr(2, 9)}`;
-export default id;
+export default function id(prefix = '_') {
+	return prefix + Math.random().toString(36).substr(2, 9);
+}
