68a5b90fc821cd5c1f651b84cf8d5853b3ecea4b
Tiger Oakes <tigeroakes@gmail.com>
2016-12-30 13:06:08 -1000

    Reimplementing event api

--- packages/page-calendar/src/pouchdb.js
+++ packages/page-calendar/src/pouchdb.js
@@ -1,6 +1,12 @@
 import PouchDB from 'pouchdb';
-// import PouchFind from 'pouchdb-find';
+import PouchFind from 'pouchdb-find';
 
-// PouchDB.plugin(PouchFind);
+PouchDB.plugin(PouchFind);
 
-export default new PouchDB('event');
+const db = new PouchDB('event');
+
+export default db;
+export const ready = Promise.all([
+	db.createIndex({ index: { fields: ['start'] } }),
+	db.createIndex({ index: { fields: ['end'] } }),
+]);
--- packages/page-calendar/src/schema/Event.js
+++ packages/page-calendar/src/schema/Event.js
@@ -1,14 +1,14 @@
 import * as Joi from 'joi';
 
-const singleSchema = Joi.object({
+const unix = Joi.number().positive().integer().description('Unix Timestamp (seconds)');
+
+export default Joi.object({
 	type: Joi.string(),
-	start: Joi.date().iso(),
-	end: Joi.date().iso(),
+	start: unix,
+	end: unix,
 	length: Joi.number().positive().integer(),
 	allDay: Joi.boolean(),
 	title: Joi.string(),
 	location: Joi.string(),
 	done: Joi.boolean(),
 });
-
-export default Joi.array().items(singleSchema);
