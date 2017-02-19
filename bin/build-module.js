7e154d3d662aa55747dd64114a2c19a347638dbf
Tiger Oakes <tigeroakes@gmail.com>
2017-02-18 22:21:53 -0800

    Working on build scripts for later use

--- bin/build-module.js
+++ bin/build-module.js
@@ -1,37 +1,14 @@
-// Build all modules in the packages/ folder
+/* eslint-disable strict,import/no-extraneous-dependencies */
 
-var path = require('path');
-var lie = require('lie');
-if (typeof Promise === 'undefined') {
-  global.Promise = lie; // required for denodeify in node 0.10
-}
-var Promise = lie;
-var denodeify = require('denodeify');
-var fs = require('fs');
-var readDir = denodeify(fs.readdir);
-var stat = denodeify(fs.stat);
+'use strict';
 
-var buildModule = require('./build-module');
-var buildPouchDB = require('./build-pouchdb');
+const { rollup } = require('rollup');
+const rollupPlugins = require('./rollupPlugins');
+const denodeify = require('denodeify');
+const { resolve } = require('path');
 
-function buildPackage(pkg) {
-  return stat(path.resolve('packages/node_modules', pkg)).then(function (stat) {
-    if (!stat.isDirectory()) { // skip e.g. 'npm-debug.log'
-      return;
-    }
-    console.log('Building ' + pkg + '...');
-    if (pkg === 'pouchdb') {
-      return buildPouchDB();
-    } else {
-      return buildModule(path.resolve('./packages/node_modules', pkg));
-    }
-  });
+function buildModules(filepath) {
+  const pkg = require(resolve(filepath, 'package.json'));
+  const topPkg = require(resolve(filepath, '../../package.json'));
 }
 
-readDir('packages/node_modules').then(function (packages) {
-  return Promise.all(packages.map(buildPackage)).catch(function (err) {
-    console.error('build error');
-    console.error(err.stack);
-    process.exit(1);
-  });
-});
