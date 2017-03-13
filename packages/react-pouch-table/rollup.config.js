8db1ee437a96caa645a4568d77128c15d6e92721
Tiger Oakes <tigeroakes@gmail.com>
2017-03-13 13:14:41 -0700

    Adding private tags to deprecated and page packages

--- packages/react-pouch-table/rollup.config.js
+++ packages/react-pouch-table/rollup.config.js
@@ -3,6 +3,11 @@ import baseConfig from '../../rollup.config.js';
 const [nodeResolve, commonjs, json, nodeGlobals,, babel] = baseConfig.plugins;
 
 export default {
+	entry: 'src/index.js',
+	targets: [
+		{ dest: 'index.mjs', format: 'es' },
+		{ dest: 'index.js', format: 'cjs' },
+	],
 	sourceMap: true,
 	plugins: [
 		nodeResolve,
