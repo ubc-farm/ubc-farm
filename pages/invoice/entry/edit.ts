b79051d662e86fcf02e77e7635f9d984894475f8
Tiger Oakes <tigeroakes@gmail.com>
2017-04-28 21:18:45 -0700

    Tweaked invoice to show error dialog

--- pages/invoice/entry/edit.ts
+++ pages/invoice/entry/edit.ts
@@ -1,5 +1,6 @@
 import { parsed } from 'document-promises';
 import { getInvoices, Invoice } from '@ubc-farm/databases';
+import { Notification } from '@ubc-farm/react-doc-editor';
 import { createElement } from 'react';
 import { render } from 'react-dom';
 import InvoiceForm from '../src/InvoiceForm';
@@ -8,20 +9,29 @@ import openInvoice from '../src/openInvoice';
 import '../www/styles/details.css';
 import '../www/styles/table.css';
 
-const dbReady = getInvoices();
-Promise.all([
-	dbReady.then(db => openInvoice(db)),
-	dbReady,
-	parsed,
-]).then(([invoice, db]) => {
+async function main() {
+	const dbReady = getInvoices();
+	const openingInvoice = dbReady.then(db => openInvoice(db));
+
+	const [db] = await Promise.all([dbReady, parsed]);
 	function back() { window.location.href = '/invoices' };
 	async function submit(i: Invoice) {
 		await db.put(i);
 		back();
 	}
 
-	render(
-		createElement(InvoiceForm, { defaultInvoice: invoice, onSubmit: submit }),
-		document.getElementById('reactRoot'),
-	);
-})
+	try {
+		const invoice = await openingInvoice;
+
+		render(
+			createElement(InvoiceForm, { defaultInvoice: invoice, onSubmit: submit }),
+			document.getElementById('reactRoot'),
+		);
+	} catch (err) {
+		render(
+			createElement(Notification, { status: 'danger', onClose: back }),
+			document.getElementById('reactRoot'),
+		)
+		console.error(err);
+	}
+}
